aed50bf4b5ab5c1542940a5624f6885c75309b58
jonasalberton <jonasceolin@gmail.com>
2023-08-30 09:36:29 -0300

    setup front-end

---
+++ frontend/src/__tests__/App.test.tsx
@@ -0,0 +1,18 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event'
+
+/**
+ * @vitest-environment jsdom
+ */
+
+test('Should find text', () => {
+  expect(1 +2 ).toBe(3);
+});
+
+test('Should click on count button and increase count number', async () => {
+  render(<App/>);
+  await userEvent.click(screen.getByText('count is 0'));
+  expect(screen.getByText('count is 1')).toBeDefined();
+})
